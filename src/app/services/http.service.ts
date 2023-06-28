c7900fb46f83920c61e9513f0dba2f4934915ef8
dasu.rahul <dasu.rahul@kellton.com>
2023-06-28 11:57:30 +0530

    add http service

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpService {
+  constructor(private http: HttpClient) {}
+
+  get(url: string): Observable<any> {
+    return this.http.get(url);
+  }
+
+  post(url: string, body: any): Observable<any> {
+    return this.http.post(url, body);
+  }
+
+  put(url: string, body: any): Observable<any> {
+    return this.http.put(url, body);
+  }
+
+  delete(url: string): Observable<any> {
+    return this.http.delete(url);
+  }
+}
